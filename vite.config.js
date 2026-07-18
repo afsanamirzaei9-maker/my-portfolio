03f5197b7a3140bc72f5dd57426e28b94794cb42
Afsana <afsanamirzaei9@gmail.com>
2026-07-18 06:00:27 +0430

    ..

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'my-portfolio'
+  // نام دقیق مخزن گیت‌هاب بین دو اسلش
+  base: '/my-portfolio/', 
+  build: {
+    // تغییر نام پوشه خروجی از dist به docs
+    outDir: 'docs', 
+  }
 })
